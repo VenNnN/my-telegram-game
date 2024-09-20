eea493590adaaefd78cbb8902cf56494644bc2d6
VeennN <v.nelin@beinf.ai>
2024-09-20 08:25:34 +0300

    upd url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://0f41-91-210-250-82.ngrok-free.app'; // Зміни на свою адресу, якщо треба
+const apiUrl = 'https://4c29-91-210-250-82.ngrok-free.app'; // Зміни на свою адресу, якщо треба
 let user_id; // Тепер user_id буде отримуватися з Telegram
 
 // Перевіряємо, чи API доступний
